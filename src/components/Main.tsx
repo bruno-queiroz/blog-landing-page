e39007a9556d551b046008709e3e58f1f375d0de
bruno <brunoqueiroz612@gmail.com>
2023-02-14 07:57:49 -0300

    Add CSS and resposiveness to Main section

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,13 +2,27 @@ import React from "react";
 
 const Main = () => {
   return (
-    <main>
-      <div>
-        <h1>A modern publishing platform</h1>
-        <p>Grow your audience and build your online brand</p>
-        <div>
-          <a href="#">Start for Free</a>
-          <a href="#">Learn More</a>
+    <main className=" ">
+      <div className="flex flex-col gap-8 items-center p-4 py-[160px] rounded-bl-[10vh] main-bg-img">
+        <h1 className="font-semibold text-white text-4xl text-center">
+          A modern publishing platform
+        </h1>
+        <p className="text-white text-md text-center">
+          Grow your audience and build your online brand
+        </p>
+        <div className="flex gap-4">
+          <a
+            href="#"
+            className="bg-white text-light-red py-3 px-6 rounded-[100vh] font-semibold text-lg"
+          >
+            Start for Free
+          </a>
+          <a
+            href="#"
+            className="text-white bg-transparent border-[2px] border-white py-3 px-6 rounded-[100vh] font-semibold text-lg"
+          >
+            Learn More
+          </a>
         </div>
       </div>
     </main>
